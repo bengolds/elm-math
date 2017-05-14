3a49edc7a1280752a3058f72886bda355bd5cf47
Ben Goldsmith <theotherben@gmail.com>
2017-05-13 19:20:37 -0700

    shows crashing area

--- src/Latex.elm
+++ src/Latex.elm
@@ -18,10 +18,12 @@ grammar =
     [ Rule (plainToken Plus) (escape "+") 1
     , Rule (plainToken Minus) (escape "-") 1
     , Rule (plainToken Times) (escape "*") 1
+    , Rule (plainToken Divide) (escape "/") 1
+    , Rule (floatToken Number) "(?:\\d*\\.)?\\d+" 1
 
-    --, Rule (plainToken Divide) (escape "/") 1
-    , Rule (plainToken Divide) "a*" 1
+    -- This line crashes elm-reactor
+    , Rule (stringToken Name) "a*" 1
 
-    --, Rule (floatToken Number) "(?:\\d*\\.)?\\d+" 1
-    --, Rule (stringToken Name) "a*" 1
+    -- This line doesn't
+    --, Rule (stringToken Name) "hello" 1
     ]
