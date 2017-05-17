edc0a67e62d57899397deaec778920499d6c059a
Ben Goldsmith <theotherben@gmail.com>
2017-05-17 00:46:07 -0700

    Most of the mathquill API works now

--- src/Mathquill.elm
+++ src/Mathquill.elm
@@ -1,62 +1,161 @@
-port module Mathquill exposing (..)
-
-import Html exposing (Html, div, text)
-import Latex exposing (..)
+--port module Mathquill exposing (editableField, subscriptions, mathquillIncoming)
+--port module Mathquill exposing (..)
 
 
-main =
-    Html.program
-        { init = init
-        , update = update
-        , view = view
-        , subscriptions = subscriptions
-        }
+module Mathquill exposing (..)
+
+import Html exposing (Html, div, text, button)
+import Html.Keyed
+import Html.Events exposing (on)
+import Html.Attributes exposing (class, property, attribute)
+import Json.Decode as Decode
 
 
-type alias Model =
-    { latexString : String
-    }
+onEdit : (String -> msg) -> Html.Attribute msg
+onEdit msg =
+    on "edit" <| Decode.map msg <| Decode.field "value" Decode.string
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( Model "", Cmd.none )
+onEnter : msg -> Html.Attribute msg
+onEnter msg =
+    on "enter" <| Decode.succeed msg
 
 
-
--- UPDATE
-
-
-type Msg
-    = InputUpdated String
+type Direction
+    = Right
+    | Left
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        InputUpdated newString ->
-            ( { model | latexString = newString }, Cmd.none )
+decodeDirection : Decode.Decoder Direction
+decodeDirection =
+    Decode.field "direction" Decode.int
+        |> Decode.map
+            (\direction ->
+                if direction < 0 then
+                    Left
+                else
+                    Right
+            )
 
 
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    mathquillEdited InputUpdated
+onMoveOutOf : (Direction -> msg) -> Html.Attribute msg
+onMoveOutOf msg =
+    on "moveOutOf" <| Decode.map msg <| decodeDirection
 
 
-port mathquillEdited : (String -> msg) -> Sub msg
+onSelectOutOf : (Direction -> msg) -> Html.Attribute msg
+onSelectOutOf msg =
+    on "selectOutOf" <| Decode.map msg <| decodeDirection
 
 
+onDeleteOutOf : (Direction -> msg) -> Html.Attribute msg
+onDeleteOutOf msg =
+    on "deleteOutOf" <| Decode.map msg <| decodeDirection
 
--- VIEW
+
+onUpOutOf : msg -> Html.Attribute msg
+onUpOutOf msg =
+    on "upOutOf" <| Decode.succeed msg
 
 
-view : Model -> Html Msg
-view model =
-    div []
-        [ div [] [ text model.latexString ]
-        , div [] [ text (toString (lexLatex model.latexString)) ]
-        ]
+onDownOutOf : msg -> Html.Attribute msg
+onDownOutOf msg =
+    on "downOutOf" <| Decode.succeed msg
+
+
+emptyAttribute : Html.Attribute msg
+emptyAttribute =
+    attribute "empty-attribute-blank-ignore" ""
+
+
+boolAttribute : String -> (Bool -> Html.Attribute msg)
+boolAttribute attrName =
+    (\value ->
+        if value then
+            attribute attrName ""
+        else
+            emptyAttribute
+    )
+
+
+spaceBehavesLikeTab : Bool -> Html.Attribute msg
+spaceBehavesLikeTab =
+    boolAttribute "space-behaves-like-tab"
+
+
+restrictMismatchedBrackets : Bool -> Html.Attribute msg
+restrictMismatchedBrackets =
+    boolAttribute "restrict-mismatched-brackets"
+
+
+sumStartsWithNEquals : Bool -> Html.Attribute msg
+sumStartsWithNEquals =
+    boolAttribute "sum-starts-with-n-equals"
+
+
+supSubsRequireOperand : Bool -> Html.Attribute msg
+supSubsRequireOperand =
+    boolAttribute "sup-subs-require-operand"
+
+
+autoSubscriptNumerals : Bool -> Html.Attribute msg
+autoSubscriptNumerals =
+    boolAttribute "auto-subscript-numerals"
+
+
+type NavigationDirection
+    = Up
+    | Down
+    | Default
+
+
+leftRightIntoCmdGoes : NavigationDirection -> Html.Attribute msg
+leftRightIntoCmdGoes dir =
+    case dir of
+        Up ->
+            attribute "left-right-into-cmd-goes" "up"
+
+        Down ->
+            attribute "left-right-into-cmd-goes" "down"
+
+        Default ->
+            emptyAttribute
+
+
+stringAttribute : String -> String -> Html.Attribute msg
+stringAttribute name string =
+    case string of
+        "" ->
+            emptyAttribute
+
+        _ ->
+            attribute name string
+
+
+charsThatBreakOutOfSupSub : String -> Html.Attribute msg
+charsThatBreakOutOfSupSub =
+    stringAttribute "chars-that-break-out-of-sup-sub"
+
+
+autoCommands : String -> Html.Attribute msg
+autoCommands =
+    stringAttribute "auto-commands"
+
+
+autoOperatorNames : String -> Html.Attribute msg
+autoOperatorNames =
+    stringAttribute "auto-operator-names"
+
+
+mathField : List (Html.Attribute msg) -> Html msg
+mathField attributes =
+    Html.Keyed.node "div"
+        (List.append attributes [ class "elm-mq-edit" ])
+        []
+
+
+staticMath : List (Html.Attribute msg) -> String -> Html msg
+staticMath attributes content =
+    Html.Keyed.node "div"
+        (List.append attributes [ class "elm-mq-static" ])
+        [ ( "content", text content ) ]
